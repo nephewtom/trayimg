909086e9dcf1a3c6fd45dbd30019631f2c00c583
nephewtom <tomasorti@gmail.com>
2025-03-23 00:41:44 +0100

    Moved rolling cube to different repo

--- build.bat
+++ build.bat
@@ -1,16 +1,5 @@
-@echo off
-del .\rolling-cube.exe
-@echo on
-g++ -g -Wall rolling-cube.cpp -o rolling-cube.exe -I./rlImGui/src -I ./rlImGui/imgui -I ./rlImGui/imgui/backends -I ./raylib/src -L./rlImGui/src -L./raylib/src -lrlImGui -lraylib -lgdi32 -lwinmm
-@echo off
-.\rolling-cube.exe
 rem -Wextra
 
-rem del .\main.exe
-rem g++ -g -Wall main.cpp -o main.exe -I./rlImGui/src -I ./rlImGui/imgui -I ./rlImGui/imgui/backends -I ./raylib/src -L./rlImGui/src -L./raylib/src -lrlImGui -lraylib -lgdi32 -lwinmm
-rem .\main.exe
-
-
-rem del .\trayimg.exe
-rem g++ -g -Wall trayimg.cpp -o trayimg.exe -I./rlImGui/src -I ./rlImGui/imgui -I ./rlImGui/imgui/backends -I ./raylib/src -L./rlImGui/src -L./raylib/src -lrlImGui -lraylib -lgdi32 -lwinmm
-rem .\trayimg.exe
+del .\main.exe
+g++ -g -Wall main.cpp -o main.exe -I./rlImGui/src -I ./rlImGui/imgui -I ./rlImGui/imgui/backends -I ./raylib/src -L./rlImGui/src -L./raylib/src -lrlImGui -lraylib -lgdi32 -lwinmm
+.\main.exe
